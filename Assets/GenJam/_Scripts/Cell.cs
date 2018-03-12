b2385498f555950f3c6eae9d9b7319079aedcb3a
Tubocass-PC\IO Dev <tubocass@gmail.com>
2018-03-12 18:32:07 -0500

    Combined some ideas behind the 2D room creation and the cellualr automata tutorials

---
+++ Assets/GenJam/_Scripts/Cell.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class Cell : MonoBehaviour 
+{
+	public Vector3 location{get{return transform.position;}set{transform.position = value;}}
+	public Sprite sprite{set{GetComponent<SpriteRenderer>().sprite = value;}}
+}
